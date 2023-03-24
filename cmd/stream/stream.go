1fc74db498746a9080c1ff44dc4f6c8650c7f498
snowmerak <snowmerak@outlook.com>
2023-03-24 19:21:21 +0900

    add stream name

--- cmd/stream/stream.go
+++ cmd/stream/stream.go
@@ -6,21 +6,23 @@ import (
 	"go/format"
 	"log"
 	"os"
+	"strings"
 )
 
 func main() {
-	if len(os.Args) < 3 {
-		log.Println("Usage: stream <package-name> <types>...")
+	if len(os.Args) < 4 {
+		log.Println("Usage: stream <package-name> <stream-name> <types>...")
 		return
 	}
 
 	packageName := os.Args[1]
-	types := os.Args[2:]
+	streamName := os.Args[2]
+	types := os.Args[3:]
 
 	buf := bytes.NewBuffer(nil)
 	buf.WriteString("package " + packageName)
 	buf.WriteString("\n\n")
-	buf.WriteString("type Stream struct {\n")
+	buf.WriteString("type " + streamName + " struct {\n")
 	for i, t := range types[:len(types)-1] {
 		buf.WriteString(fmt.Sprintf("\tF%d func(%s) (%s, error)\n", i+1, t, types[i+1]))
 	}
@@ -28,15 +30,15 @@ func main() {
 
 	buf.WriteString("\n")
 
-	buf.WriteString("func NewStream(")
+	buf.WriteString("func New" + streamName + "(")
 	for i, t := range types[:len(types)-1] {
 		if i > 0 {
 			buf.WriteString(", ")
 		}
 		buf.WriteString(fmt.Sprintf("f%d func(%s) (%s, error)", i+1, t, types[i+1]))
 	}
-	buf.WriteString(") *Stream {\n")
-	buf.WriteString("\treturn &Stream{\n")
+	buf.WriteString(") *" + streamName + " {\n")
+	buf.WriteString("\treturn &" + streamName + "{\n")
 	for i := range types[:len(types)-1] {
 		buf.WriteString(fmt.Sprintf("\t\tF%d: f%d,\n", i+1, i+1))
 	}
@@ -45,7 +47,7 @@ func main() {
 
 	buf.WriteString("\n")
 
-	buf.WriteString("func (s *Stream) Run(")
+	buf.WriteString("func (s *" + streamName + ") Run(")
 	buf.WriteString(fmt.Sprintf("init %s", types[0]))
 	buf.WriteString(") (")
 	buf.WriteString(fmt.Sprintf("result %s", types[len(types)-1]))
@@ -65,7 +67,7 @@ func main() {
 		log.Fatal(err)
 	}
 
-	f, err := os.Create("./stream.go")
+	f, err := os.Create("./" + strings.ToLower(streamName) + ".go")
 	if err != nil {
 		log.Fatal(err)
 	}
