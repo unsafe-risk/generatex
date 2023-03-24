57e962946953ea0934bc4b038e6588153598d442
snowmerak <snowmerak@outlook.com>
2023-03-24 18:03:16 +0900

    init project

---
+++ cmd/helloworld/helloworld.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"bytes"
+	"log"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		log.Println("Usage: helloworld <package-name>")
+		return
+	}
+	packageName := os.Args[1]
+
+	f, err := os.Create("./helloworld.go")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	buf := bytes.NewBuffer(nil)
+	buf.WriteString("package " + packageName)
+	buf.WriteString("\n\n")
+	buf.WriteString("import fmt\n\n")
+	buf.WriteString("func HelloWorld() string {\n")
+	buf.WriteString("\treturn \"Hello World\"\n")
+	buf.WriteString("}\n")
+
+	_, err = f.Write(buf.Bytes())
+	if err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/unsafe-risk/generatex
+
+go 1.20
